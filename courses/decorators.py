63f7a1bd0d811918366d68176f0190fb77fc6de1
= <araariadem0@gmail.com>
2020-03-15 23:10:08 +0100

    Decorators updated

--- courses/decorators.py
+++ courses/decorators.py
@@ -1,18 +1,40 @@
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from courses.models import Course
+from users.models import Teacher
+
+def is_teacher(user):
+	return user.is_authenticated and (user.is_teacher or user.is_superuser)
+
 def teacher_required(func):
-	def wrapper(*args, **kwargs):
 
-		value = func(*args, **kwargs)
+	def wrapper(request, *args, **kwargs):
+		user = request.user
 
-		return value
+		if is_teacher(user):
+			value = func(request, *args, **kwargs)
+			return value
+		
+		raise PermissionDenied()
 
 	return wrapper
 
 
 def teacher_owner_required(func):
-	def wrapper(*args, **kwargs):
 
-		value = func(*args, **kwargs)
+	def wrapper(request, course_slug, *args, **kwargs):
+		user = request.user
 
-		return value
+		if is_teacher(user):
+			course = get_object_or_404(Course, slug=course_slug)
+			teacher = get_object_or_404(Teacher, user=user)
+
+			if course.teacher == teacher:
+				value = func(request, course_slug, *args, **kwargs)
+
+				return value
+
+		raise PermissionDenied()
 
 	return wrapper
