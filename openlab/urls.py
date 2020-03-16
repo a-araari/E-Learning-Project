f93337a4761eb9a5184bb9e9681c7db376aa1512
= <araariadem0@gmail.com>
2020-03-16 16:17:24 +0100

    ckeditor WYSIWYG added

--- openlab/urls.py
+++ openlab/urls.py
@@ -4,6 +4,7 @@ from home import views as home_views
 from django.conf import settings
 from django.conf.urls.static import static
 from courses.views import CourseList
+from ckeditor_uploader import views as ckeditor_views
 
 
 urlpatterns = [
@@ -17,6 +18,10 @@ urlpatterns = [
 
     path('accounts/', include('users.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    # CKEditor urls, don' user include to remove @staff_user_required!
+    path('upload/', ckeditor_views.upload, name='ckeditor_upload'),
+    path('browse/', ckeditor_views.browse, name='ckeditor_browse'),
 
 ]
 
